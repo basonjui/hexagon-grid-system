f39d3ccfdd3568b9f48b56db8a0e074111cd511a
Quan Bui <basonjui@gmail.com>
2022-06-01 18:18:02 +0700

    integrate Gson to serialize & deserialize

--- src/main/java/com/masterisehomes/geometryapi/App.java
+++ src/main/java/com/masterisehomes/geometryapi/App.java
@@ -2,7 +2,11 @@ package com.masterisehomes.geometryapi;
 
 import static spark.Spark.*;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.Gson.*;
 import com.masterisehomes.geometryapi.hexagon.*;
 
 /**
@@ -12,21 +16,31 @@ import com.masterisehomes.geometryapi.hexagon.*;
 public class App {
 
     public static void main(String[] args) {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Gson gson = new Gson();
 
-        get("/api/hexagon", (req, res) -> {
-            Double latitude = Double.parseDouble(req.queryParams("latitude"));
-            Double longitude = Double.parseDouble(req.queryParams("longitude"));
-            Integer circumradius = Integer.parseInt(req.queryParams("radius"));
 
-            Coordinates hexCoord = new Coordinates(latitude, longitude);
-            Hexagon hexagon = new Hexagon(hexCoord, circumradius);
+        String jsonString = "{\"latitude\": 150, \"longitude\": 150, \"radius\": 100}";
 
-            return gson.toJson(hexagon.getVertices());
-        });
+        JsonObject jsonObject = gson.fromJson(jsonString, JsonObject.class);
 
-        get("/api/neighbors", (req, res) -> {
-            return "Neighbor API Endpoint";
-        });
+
+        System.out.println(jsonObject.get("latitude").getAsDouble());
+
+        // post("/api/hexagon", "application/json", (req, res) -> {
+        //     Double latitude = Double.parseDouble(req.queryParams("latitude"));
+        //     Double longitude = Double.parseDouble(req.queryParams("longitude"));
+        //     Integer circumradius = Integer.parseInt(req.queryParams("radius"));
+
+        //     Coordinates hexCoord = new Coordinates(latitude, longitude);
+        //     Hexagon hexagon = new Hexagon(hexCoord, circumradius);
+
+            
+
+        //     return gson.toJson(req.body());
+        // });
+
+        // get("/api/neighbors", (req, res) -> {
+        //     return "Neighbor API Endpoint";
+        // });
     }
 }
