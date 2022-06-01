2324c13d0af054ec5bfb182e8e790b4fecafec24
Quan Bui <basonjui@gmail.com>
2022-06-01 15:58:40 +0700

    fix typos in comments

--- src/main/java/com/masterisehomes/geometryapi/hexagon/Coordinates.java
+++ src/main/java/com/masterisehomes/geometryapi/hexagon/Coordinates.java
@@ -1,84 +1,82 @@
 package com.masterisehomes.geometryapi.hexagon;
 
- /*
- So whlongitude the Vertex class instead of using the predefined Point class?
+/*
+So why the Vertex class instead of using the predefined Point class?
+Point is a really old class in Java, it can only work with Integer and Double.
 
- Point is a really old class in Java,
- it can onllongitude work with Integer and Double.
+This is a problem because the vertex() function in processing can
+only work with Float data type.
 
- This is a problem because the vertex() function in processing can
- only work with Float data type.
- 
- Do we want to cast Double to Float for everlongitude point to draw on Processing?
- So here we are.
- */
+Do we want to cast Double to Float for every point to draw on Processing?
+So here we are.
+*/
 
 public class Coordinates {
-    private double latitude;
-    private double longitude;
-    
-    // Constructors
-    public Coordinates(double latitude, double longitude) {
-      this.latitude = latitude;
-      this.longitude = longitude;
-    }
-    
-    // Comparison methods
-    public boolean isEqual(Coordinates coord) {
-      double latitude = coord.getLatitude();
-      double longitude = coord.getLongitude();
-      
-      if (this.latitude == latitude && this.longitude == longitude) {
-        return true;
-      } else {
+  private double latitude;
+  private double longitude;
+
+  // Constructors
+  public Coordinates(double latitude, double longitude) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+
+  // Comparison methods
+  public boolean isEqual(Coordinates coord) {
+    double latitude = coord.getLatitude();
+    double longitude = coord.getLongitude();
+
+    if (this.latitude == latitude && this.longitude == longitude) {
+      return true;
+    } else {
       return false;
-      }
-    }
-    
-    public boolean isLarger(Coordinates coord) {
-      double latitude = coord.getLatitude();
-      double longitude = coord.getLongitude();
-      
-      if (this.latitude > latitude && this.longitude > longitude) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-    
-    public boolean isSmaller(Coordinates coord) {
-      double latitude = coord.getLatitude();
-      double longitude = coord.getLongitude();
-      
-      if (this.latitude < latitude && this.longitude < longitude) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-    
-    public boolean isIn(Boundary b) {
-      Coordinates bStart = b.getStart();
-      Coordinates bEnd = b.getEnd();
-      
-      if (this.isLarger(bStart) && this.isSmaller(bEnd)) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-    
-    // Getters
-    public double getLatitude() {
-      return this.latitude;
-    }
-  
-    public double getLongitude() {
-      return this.longitude;
-    }
-    
-    // String representation
-    public String toString() {
-      return String.format("Coordinates[latitude=%s, longitude=%s]", this.latitude, this.longitude);
     }
   }
+
+  public boolean isLarger(Coordinates coord) {
+    double latitude = coord.getLatitude();
+    double longitude = coord.getLongitude();
+
+    if (this.latitude > latitude && this.longitude > longitude) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean isSmaller(Coordinates coord) {
+    double latitude = coord.getLatitude();
+    double longitude = coord.getLongitude();
+
+    if (this.latitude < latitude && this.longitude < longitude) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean isIn(Boundary b) {
+    Coordinates bStart = b.getStart();
+    Coordinates bEnd = b.getEnd();
+
+    if (this.isLarger(bStart) && this.isSmaller(bEnd)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // Getters
+  public double getLatitude() {
+    return this.latitude;
+  }
+
+  public double getLongitude() {
+    return this.longitude;
+  }
+
+  // String representation
+  public String toString() {
+    return String.format("Coordinates[latitude=%s, longitude=%s]", this.latitude, this.longitude);
+  }
+}
