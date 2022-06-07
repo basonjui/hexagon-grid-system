ee40c52a4cd2f47e072882714eb6adc52913c26f
Quan Bui <basonjui@gmail.com>
2022-06-07 12:09:19 +0700

    redefine type for Geometry class & set case switch foundation for constructor

--- src/main/java/com/masterisehomes/geometryapi/geojson/Geometry.java
+++ src/main/java/com/masterisehomes/geometryapi/geojson/Geometry.java
@@ -3,12 +3,24 @@ package com.masterisehomes.geometryapi.geojson;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Geometry extends GeoJsonDataType{
-    private List<Object> coordinates; // store List<List<Double>>
+    private List<?> coordinates = new ArrayList<>(); // store List<List<Double>>
 
     public Geometry(String type) {
         super();
         checkType(type);
         this.type = type;
+
+        switch(this.type) {
+            case "Point":
+            ;
+
+            case "LineString":
+            ;
+
+            case "Polygon":
+            ;
+        }
     }
 }
