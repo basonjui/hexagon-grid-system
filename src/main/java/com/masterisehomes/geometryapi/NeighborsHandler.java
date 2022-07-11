837e48088b75a3e7a5e1834380ed1d1b9c520253
Quan Bui <basonjui@gmail.com>
2022-07-11 18:39:32 +0700

    implement NeighborsHandler with Lambda's RequestHandler interface

---
+++ src/main/java/com/masterisehomes/geometryapi/NeighborsHandler.java
@@ -0,0 +1,38 @@
+package com.masterisehomes.geometryapi;
+
+import java.util.Map;
+
+import com.google.gson.Gson;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import com.masterisehomes.geometryapi.neighbors.NeighborsDto;
+import com.masterisehomes.geometryapi.geojson.GeoJsonManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NeighborsHandler implements RequestHandler<Map<String, Object>, String> {
+    private static final Logger logger = LoggerFactory.getLogger(NeighborsHandler.class);
+    private static final Gson gson = new Gson();
+
+    @Override
+    public String handleRequest(Map<String, Object> event, Context context) {
+        // log execution details
+        logger.info("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
+        logger.info("CONTEXT: " + gson.toJson(context));
+
+        // process event
+        logger.info("EVENT TYPE: " + event.getClass().toString());
+        logger.info("EVENT KEYS: " + event.keySet());
+        logger.info("EVENT: " + gson.toJson(event));
+
+        // Parse event map to DTO to extract and store data
+        NeighborsDto dto = new NeighborsDto(event);
+
+        GeoJsonManager manager = new GeoJsonManager(dto.getNeighbors());
+
+        return gson.toJson(manager.getFeatureCollection());
+    }
+}
