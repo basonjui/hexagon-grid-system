1d9103e2aa17e8abf63507f3d2f9692707183ea7
Quan Bui <basonjui@gmail.com>
2022-07-18 17:51:49 +0700

    create HexagonDirections enum & implement enum switch case skeleton

--- src/main/java/com/masterisehomes/geometryapi/index/CubeCoordinatesIndex.java
+++ src/main/java/com/masterisehomes/geometryapi/index/CubeCoordinatesIndex.java
@@ -10,17 +10,38 @@ public class CubeCoordinatesIndex {
     @Getter
     private int s;
 
-    public CubeCoordinatesIndex(CubeCoordinatesIndex previousIndex, int hexagonDirection) {
-        switch (hexagonDirection) {
-            case 0: // default case - where previousIndex == null & hexagonDirection == 0 (FYI)
+    public CubeCoordinatesIndex(CubeCoordinatesIndex previousIndex, HexagonDirection generatedDirection) {
+        switch (generatedDirection) {
+            case NONE: // default case - where previousIndex == null & generatedDirection == 0 (FYI)
                 this.q = 0;
                 this.r = 0;
                 this.s = 0;
                 break;
 
-            case 1:
-                // logic to calculate new index base on previousIndex & hexagonDirection
+            case ONE:
+                // logic to calculate new index base on previousIndex & generatedDirection
                 break;
+                
+            case TWO:
+                // logic to calculate new index base on previousIndex & generatedDirection
+                break;
+
+            case THREE:
+                // logic to calculate new index base on previousIndex & generatedDirection
+                break;
+
+            case FOUR:
+                // logic to calculate new index base on previousIndex & generatedDirection
+                break;
+
+            case FIVE:
+                // logic to calculate new index base on previousIndex & generatedDirection
+                break;
+
+            case SIX:
+                // logic to calculate new index base on previousIndex & generatedDirection
+                break;
+
         }
     }
 }
