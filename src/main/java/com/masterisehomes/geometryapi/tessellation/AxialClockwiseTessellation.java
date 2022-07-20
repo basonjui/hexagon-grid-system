b6e0c6c8f923fa3c5ad8e4e976d3c2835cdd1fa0
Quan Bui <basonjui@gmail.com>
2022-07-20 14:33:02 +0700

    clean up AxialClockwiseTessellation: remove unused fields

--- src/main/java/com/masterisehomes/geometryapi/tessellation/AxialClockwiseTessellation.java
+++ src/main/java/com/masterisehomes/geometryapi/tessellation/AxialClockwiseTessellation.java
@@ -2,10 +2,11 @@ package com.masterisehomes.geometryapi.tessellation;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import lombok.Getter;
 import lombok.ToString;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import com.masterisehomes.geometryapi.hexagon.Coordinates;
 import com.masterisehomes.geometryapi.hexagon.Hexagon;
@@ -24,50 +25,39 @@ public class AxialClockwiseTessellation {
     // Computations
     @Getter
     private final Hexagon rootHexagon;
-    @Getter
-    private final Neighbors neighbors;
-    // The axial origin of our Tessellation algorithm, Neighbors is nthRing: 0
-    private final Map<Integer, Coordinates> neighborsCentroids;
-    private final Map<Integer, Coordinates> neighborsGisCentroids;
 
-    // Storage
     /*
+     * Storage
+     *
      * Notes: ArrayList needs to be assigned initialCapacity for better performance,
-     * it cuts the cyle to expand the Array when it is full (default initialCapacity = 10)
+     * it cuts the cyle to expand the Array when it is full (default initialCapacity
+     * = 10)
      */
     @Getter
-    private List<Coordinates> tessellationCentroids = new ArrayList<Coordinates>(100);
+    private List<Coordinates> centroids = new ArrayList<Coordinates>(100);
     @Getter
-    private List<Coordinates> tessellationGisCentroids = new ArrayList<Coordinates>(100);
+    private List<Coordinates> gisCentroids = new ArrayList<Coordinates>(100);
     @Getter
-    private List<Hexagon> tessellationHexagons = new ArrayList<Hexagon>(100);
+    private List<Hexagon> hexagons = new ArrayList<Hexagon>(100);
     @Getter
-    private List<Hexagon> tessellationGisHexagons = new ArrayList<Hexagon>(100);
+    private List<Hexagon> gisHexagons = new ArrayList<Hexagon>(100);
 
     // Updater
     @Getter
-    private int rings = tessellationCentroids.size();
+    private int totalRings;
     @Getter
-    private int nthRing = 0;
+    private int nthRing;
 
     public AxialClockwiseTessellation(Coordinates origin, double circumradius) {
         this.origin = origin;
         this.circumradius = circumradius;
-
         this.rootHexagon = new Hexagon(this.origin, this.circumradius);
-        this.neighbors = new Neighbors(this.rootHexagon);
-        this.neighborsCentroids = this.neighbors.getCentroids();
-        this.neighborsGisCentroids = this.neighbors.getGisCentroids();
     }
 
     public AxialClockwiseTessellation(Hexagon rootHexagon) {
         this.rootHexagon = rootHexagon;
-
         this.origin = this.rootHexagon.getCentroid();
         this.circumradius = this.rootHexagon.getCircumradius();
-        this.neighbors = new Neighbors(this.rootHexagon);
-        this.neighborsCentroids = this.neighbors.getCentroids();
-        this.neighborsGisCentroids = this.neighbors.getGisCentroids();
     }
 
     public void tessellate(Boundary boundary) {
@@ -75,19 +65,22 @@ public class AxialClockwiseTessellation {
         this.boundary = boundary;
 
         // Check nthRing
-        switch(this.nthRing) {
+        switch (this.nthRing) {
             case 0:
-            // 
-            break;
+                //
+                break;
         }
     }
 
     public static void main(String[] args) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
         Coordinates origin = new Coordinates(10, 10);
         Hexagon hexagon = new Hexagon(origin, 5000);
         AxialClockwiseTessellation tessellation = new AxialClockwiseTessellation(hexagon);
-        
+
         Neighbors neighbors = new Neighbors(hexagon);
-        System.out.println(neighbors.getGisCentroids());
+        // System.out.println(neighbors.getGisCentroids());
+        System.out.println(gson.toJson(tessellation));
     }
 }
