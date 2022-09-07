b6ac724e0659bbd23d42985f5a7619b83b5c46ab
Quan Bui <basonjui@gmail.com>
2022-09-07 12:44:40 +0700

    implement getConnection() for PostgresJDBC & fix bug in generateJDBCUrl()

--- src/main/java/com/masterisehomes/geometryapi/database/PostgresJDBC.java
+++ src/main/java/com/masterisehomes/geometryapi/database/PostgresJDBC.java
@@ -3,94 +3,103 @@ package com.masterisehomes.geometryapi.database;
 import java.sql.*;
 import java.util.Properties;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
 public class PostgresJDBC {
         private static final String DBMS_URL = "jdbc:postgresql:";
 
-        private String DATABASE;
-        private String HOST;
-        private int PORT; // default == 0
-        
-        private String JDBC_URL;
+        @Setter
+        private String database;
+        @Setter
+        private String host;
+        @Setter
+        private int port; // default == 0
 
-        public void setDatabase(String database) {
-                this.DATABASE = database;
-        }
-
-        public void setHost(String host) {
-                this.HOST = host;
-        }
-
-        public void setPort(int port) {
-                this.PORT = port;
-        }
+        @Setter
+        private String username;
+        @Setter
+        @Getter(AccessLevel.NONE)
+        private String password;
+        private String pgjdbcUrl;
 
         private String generateJDBCUrl() {
-                final StringBuilder URL_BUILDER = new StringBuilder();
+                final StringBuilder urlBuilder = new StringBuilder();
+                urlBuilder.append(DBMS_URL);
 
-                if (this.HOST.isEmpty()) {
-                        if (this.DATABASE.isEmpty()) {
+                if (this.host == null) {
+                        /* No host */
+                        if (this.database == null) {
                                 // jdbc:postgresql:/
-                                URL_BUILDER.append(DBMS_URL).append("/");
+                                urlBuilder.append("/");
                         } else {
                                 // jdbc:postgresql:database
-                                URL_BUILDER.append(this.DATABASE);
+                                urlBuilder.append(this.database);
                         }
                 } else {
                         // jdbc:postgresql://host
-                        URL_BUILDER.append("//").append(this.HOST);
+                        urlBuilder.append("//").append(this.host);
 
-                        if (this.DATABASE.isEmpty()) {
-                                /* If there is no specified PORT */
-                                if (this.PORT == 0) {
+                        if (this.database == null) {
+                                /* No port */
+                                if (this.port == 0) {
                                         // jdbc:postgresql://host/
-                                        URL_BUILDER.append("/");
+                                        urlBuilder.append("/");
                                 } else {
                                         // jdbc:postgresql://host:port/
-                                        URL_BUILDER.append(":").append(this.PORT)
+                                        urlBuilder.append(":").append(this.port)
                                                         .append("/");
                                 }
 
                         } else {
-                                if (this.PORT == 0) {
+                                /* There is port */
+                                if (this.port == 0) {
                                         // jdbc:postgresql://host/database
-                                        URL_BUILDER.append("/").append(this.DATABASE);
+                                        urlBuilder.append("/").append(this.database);
                                 } else {
                                         // jdbc:postgresql://host:port/database
-                                        URL_BUILDER.append(":").append(this.PORT)
-                                                        .append("/").append(this.DATABASE);
+                                        urlBuilder.append(":").append(this.port)
+                                                        .append("/").append(this.database);
                                 }
                         }
                 }
 
-                return URL_BUILDER.toString();
+                return urlBuilder.toString();
         }
 
-        public Connection getConnection() throws SQLException {
-                // Check if JDBC_URL is ready
-                if (this.JDBC_URL.isEmpty()) {
-                        this.JDBC_URL = generateJDBCUrl();
+        public Connection getConnection() {
+                // Check if pgjdbcUrl is ready
+                if (this.pgjdbcUrl == null) {
+                        this.pgjdbcUrl = generateJDBCUrl();
                 }
-                
+
                 Connection conn = null;
-                // Properties connectionProps = new Properties();
-                // connectionProps.put("user", this.userName);
-                // connectionProps.put("password", this.password);
+                try {
+                        conn = DriverManager.getConnection(this.pgjdbcUrl, this.username, this.password);
+                        System.out.println("Connected to the PostgreSQL server successfully.");
+                } catch (SQLException e) {
+                        System.out.println(e.getMessage());
+                }
 
-                // if (this.dbms.equals("mysql")) {
-                //         conn = DriverManager.getConnection(
-                //                         "jdbc:" + this.dbms + "://" +
-                //                                         this.serverName +
-                //                                         ":" + this.portNumber + "/",
-                //                         connectionProps);
-                // } else if (this.dbms.equals("derby")) {
-                //         conn = DriverManager.getConnection(
-                //                         "jdbc:" + this.dbms + ":" +
-                //                                         this.dbName +
-                //                                         ";create=true",
-                //                         connectionProps);
-                // }
-
-                System.out.println("Connected to database");
                 return conn;
+
+        }
+
+        public static void main(String[] args) {
+                PostgresJDBC pg = new PostgresJDBC();
+                pg.setHost("10.10.12.197");
+                pg.setPort(5432);
+                pg.setDatabase("raw_db");
+
+                pg.setUsername("quan.bui");
+                pg.setPassword("Q.bui95@masterise");
+
+                System.out.println("\nJDBC Url: " + pg.generateJDBCUrl());
+
+                pg.getConnection();
         }
 }
